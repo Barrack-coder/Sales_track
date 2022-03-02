ea23b4a8a3f3d73f3497bb3fa9ca6da147f9e0b7
DannyMyles <dannymyles1999@gmail.com>
2022-03-02 08:34:18 +0300

    added git ignore

--- sales/settings.py
+++ sales/settings.py
@@ -74,13 +74,15 @@ WSGI_APPLICATION = 'sales.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
